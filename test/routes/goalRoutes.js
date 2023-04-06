de48d44d8b85a0d61f0c14fcf5355f12532412f8
Saad Ahmad <saad.ahmad1024@gmail.com>
2023-04-06 13:49:42 +0500

    hell noooo

--- test/routes/goalRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getGoals , setGoal, updateGoal, deletGoal } = require('../controllers/goalController')
-
-router.route('/').get(getGoals).post(setGoal)
-
-router.route('/:id').put(updateGoal).delete(deletGoal)
-
-module.exports = router
