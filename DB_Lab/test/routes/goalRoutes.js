8c48a65db26a07b1203ea7d2df8dbc07e6ca47db
Saad Ahmad <saad.ahmad1024@gmail.com>
2023-04-06 13:49:59 +0500

    helll yess

---
+++ DB_Lab/test/routes/goalRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const { getGoals , setGoal, updateGoal, deletGoal } = require('../controllers/goalController')
+
+router.route('/').get(getGoals).post(setGoal)
+
+router.route('/:id').put(updateGoal).delete(deletGoal)
+
+module.exports = router
