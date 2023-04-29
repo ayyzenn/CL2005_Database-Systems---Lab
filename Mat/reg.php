2d77e3a9de1e13aa4d214ccca4b4492605669930
Saad Ahmad <saad.ahmad1024@gmail.com>
2023-04-29 11:00:48 +0500

    fkddfkldkfldsfk

---
+++ Mat/reg.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="registration.php" method="GET">
+parkcode <input type="text" name="PARK_CODE"><br>
+parkname  <input type="text" name="PARK_NAME"><br>
+parkcity  <input type="text" name="PARK_CITY"><br>
+parkcountry <input type="text" name="PARK_COUNTRY"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
